cdaa66f4cd01d2028a53b3433d6b1e5203e80454
Jill Hubley <jill.hubley@gmail.com>
2020-01-13 17:54:48 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import MapGL from 'react-map-gl';
+import {MapStylePicker} from './controls';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiamFodWJsZXkiLCJhIjoiY2pieTI1enRiMnlqZzJ3cWhjMTc3eG5odyJ9.V4VKp7ThWOF3svsXOP7H9A'
+
+export default class App extends Component {
+  state = {
+    style: 'mapbox://styles/mapbox/light-v9',
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      longitude: -74,
+      latitude: 40.7,
+      zoom: 11,
+      maxZoom: 16
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this._resize);
+    this._resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this._resize);
+  }
+
+  onStyleChange = (style) => {
+    this.setState({style});
+  }
+
+  _onViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    });
+  }
+
+  _resize = () => {
+    this._onViewportChange({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <MapStylePicker onStyleChange={this.onStyleChange} currentStyle={this.state.style}/>
+        <MapGL
+          {...this.state.viewport}
+          mapStyle={this.state.style}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          onViewportChange={viewport => this._onViewportChange(viewport)}
+        >
+        </MapGL>
+      </div>
+    );
+  }
+}
